983cc0b915e77c5c97b268db59ae458e1b6bd667
ahmetkeban@gmail.com <AhmetAkpunar>
2023-01-09 18:25:29 +0300

    password maskes and screenshot taken

---
+++ src/test/java/tests/Case006_PasswordMasked.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.testng.annotations.Test;
+import pages.PracticeAutomation;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+import java.io.IOException;
+
+public class Case006_PasswordMasked {
+
+    @Test(testName = "Password Should be Masked")
+    public void test01() throws IOException {
+
+        // 1) Open the browser
+        // 2) Enter the URL “http://practice.automationtesting.in/”
+        Driver.getDriver().get("http://practice.automationtesting.in/");
+        // 3) Click on My Account Menu
+        PracticeAutomation practiceAutomation = new PracticeAutomation();
+        practiceAutomation.myAccountLink.click();
+        Driver.getDriver().navigate().refresh();
+        ReusableMethods.waitForIt(2);
+        practiceAutomation.myAccountLink.click();
+        // 4) Enter the password field with some characters.
+        practiceAutomation.password.sendKeys("wow!");
+        // 5) The password field should display the characters in asterisks or bullets such that
+        // the password is not visible on the screen
+        ReusableMethods.getScreenshot("Password masked");
+        // Screenshot saved to target/Screenshots/Password masked20230109062154.png
+
+    }
+}
